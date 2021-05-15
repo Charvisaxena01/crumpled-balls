b83d3c37fc5455277ef8f665fab1d3705dbef7bb
Charvisaxena01 <74494231+Charvisaxena01@users.noreply.github.com>
2021-05-15 18:57:38 +0530

    Add files via upload

---
+++ ground.js
@@ -0,0 +1,27 @@
+class Ground  {
+    constructor(x,y,width,height){
+       var options={
+            isStatic:true
+        }
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    this.body = Bodies.rectangle(x,y,this.width,this.height,options)
+        World.add(world,this.body)
+       
+    }
+    display(){
+    
+        var groundpos =this.body.position;
+      
+        push();
+        translate(groundpos.x, groundpos.y);
+        
+       // strokeWeight(4);
+        rectMode(CENTER);
+        fill("red");
+        rect(0, 0, this.width,this.height );
+        pop();
+    }
+    }
